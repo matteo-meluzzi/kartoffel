fc86d5a195756c0f2ca1b3c5881af153ad7eaedf
Suffix Trie <email>
2025-02-21 17:37:54 +0100

    doesnt work

--- src/main.rs
+++ src/main.rs
@@ -2,16 +2,19 @@
 #![no_main]
 
 use kartoffel::*;
-use threat_map::{coordinates::BotCentricCoordinate, greedy_next_move::greedy_next_move, threat_map::ThreatMap, Move};
+use threat_map::{coordinates::Coordinate, greedy_next_move::greedy_next_move, orientation::Orientation, threat_map::ThreatMap, direction::Direction};
 
 struct Robot {
     threat_map: ThreatMap,
-    enemy_positions: [BotCentricCoordinate; 9 * 9]
+    position_in_threat_map: Coordinate,
+    enemy_positions: [Coordinate; 9 * 9],
+    orientation: Orientation
 }
 
 impl Robot {
     fn round_robin_step(&mut self) {    
         if is_radar_ready() {
+            self.position_in_threat_map = Coordinate::new(0, 0);
             let scan = radar_scan_9x9();
             // for y in -4..=4 {
             //     for x in -4..=4 {
@@ -23,25 +26,29 @@ impl Robot {
             // println!("");
 
             let enemy_count = self.calculate_enemy_positions(&scan);
+            self.threat_map.update_orientation(self.orientation);
             self.threat_map.calculate(&self.enemy_positions[0..enemy_count]);
             self.threat_map.mask_border(&|coord| { scan.at(coord.x, coord.y) == ' ' });
         } 
 
         if is_motor_ready() {
-            if let Some(mov) = greedy_next_move(&self.threat_map) {
-                // println!("{:?}", mov);
+            if let Some(mov) = greedy_next_move(&self.threat_map, self.orientation, self.position_in_threat_map) {
                 match mov {
-                    Move::Front => motor_step_fw(),
-                    Move::Back => motor_step_bw(),
-                    Move::Right => {
-                        motor_turn_right();
-                        motor_wait();
-                        motor_step_fw();
+                    Direction::Front => {
+                        self.position_in_threat_map = self.position_in_threat_map.in_direction(self.orientation.relative_to(self.threat_map.orientation));
+                        motor_step_fw()
                     }
-                    Move::Left => {
+                    Direction::Back => {
+                        self.position_in_threat_map = self.position_in_threat_map.in_direction(self.orientation.rotated_right().rotated_right().relative_to(self.threat_map.orientation));
+                        motor_step_bw()
+                    }
+                    Direction::Right => {
+                        motor_turn_right();
+                        self.orientation = self.orientation.rotated_right()
+                    }
+                    Direction::Left => {
                         motor_turn_left();
-                        motor_wait();
-                        motor_step_fw();
+                        self.orientation = self.orientation.rotated_left()
                     }
                 }
             } else {
@@ -59,7 +66,7 @@ impl Robot {
                     continue;
                 }
                 if scan.at(x, y) == '@' {
-                    self.enemy_positions[enemy_count] = BotCentricCoordinate{x, y};
+                    self.enemy_positions[enemy_count] = Coordinate{x, y};
                     enemy_count += 1;
                 }
             }
@@ -70,7 +77,8 @@ impl Robot {
 
 #[no_mangle]
 fn main() {
-    let mut robot = Robot{ threat_map: ThreatMap::new(), enemy_positions: [BotCentricCoordinate::new(0, 0); 9 * 9] };
+    let orientation = Orientation::from_integer(compass_dir() as i32 - 1).unwrap();
+    let mut robot = Robot{ threat_map: ThreatMap::new(orientation), enemy_positions: [Coordinate::new(0, 0); 9 * 9], orientation, position_in_threat_map: Coordinate::new(0, 0) };
     loop {
         robot.round_robin_step();
     }
