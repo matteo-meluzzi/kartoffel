e59e3e65b1aa7ed84ea3bf2898db51aeead9b480
Suffix Trie <email>
2025-03-30 12:01:14 +0200

    rl

--- src/main.rs
+++ src/main.rs
@@ -3,20 +3,16 @@
 
 mod kartoffel_nn;
 
-use core::mem::swap;
 use kartoffel::*;
-use threat_map::{borders::Borders, coordinates::Coordinate, direction::Direction, enemy_position::{EnemyPosition, EnemyPositions}, orientation::Orientation, robot_position::RobotPosition, N};
 use kartoffel_nn::{KARTOFFEL_NN, Fix};
+
+const N: usize = 7;
 
 fn argmax<T: Ord>(xs: &[T]) -> Option<usize> {
     xs.iter().enumerate().max_by_key(|(_, t)| *t).map(|(i, _)| i)
 }
 
 struct Robot {
-    enemy_positions: EnemyPositions,
-    previous_enemy_positions: EnemyPositions,
-    robot_position: RobotPosition,
-    borders: Borders
 }
 
 fn print_scan<const N:usize>(scan: &RadarScan<N>) {
@@ -32,130 +28,70 @@ fn print_scan<const N:usize>(scan: &RadarScan<N>) {
 }
 
 impl Robot {
-    fn radar_is_ready(&mut self) {
-        let scan = radar_scan_9x9();
-        // println!("{:?}", self.robot_position.orientation);
-        print_scan(&scan);
+    fn get_observations(&self, scan: &RadarScan<N>) -> [Fix; 50] {
+        let mut observations = [Fix::ZERO; 50];
 
-        // the current positions become old
-        swap(&mut self.previous_enemy_positions, &mut self.enemy_positions);
+        let n = threat_map::N as i8;
+        for i in 0..49 {
+            let x = i%n - n/2;
+            let y = i/n - n/2;
+            let index = i as usize;
 
-        // ajdust coordinate system of previous enemies
-        self.previous_enemy_positions.use_origin(self.robot_position.position);
-
-        // get the new positions
-        (self.enemy_positions, self.borders) = self.analyze_scan(&scan);
-        
-        // we are now at the center of the coordinate system again
-        self.robot_position.position = Coordinate::new(0, 0);
-    }
-
-    fn get_observations(&self) -> [Fix; 244] {
-        let mut observations = [Fix::ZERO; 244];
-
-        for enemy_pos in &self.enemy_positions {
-            let observations = &mut observations[..N*N];
-            let index = enemy_pos.position.to_index().expect("current enemy position is out of the scan");
-            observations[index] = Fix::ONE;
-        }
-
-        for prev_enemy_pos in &self.previous_enemy_positions {
-            let observations = &mut observations[N*N..N*N*2];
-            if let Some(index) = prev_enemy_pos.position.to_index() {
+            let c = scan.at(x, y);
+            if c == ' ' || c == '@' {
                 observations[index] = Fix::ONE;
-            }
-        }
-
-        let border_observations = &mut observations[N*N*2..N*N*3];
-        let mut border_observations_index = 0;
-        for y in 0..N as i8 {
-            for x in 0..N as i8 {
-                let coord = Coordinate::new(x, y);
-                if self.borders.is_border(coord) {
-                    border_observations[border_observations_index] = Fix::ONE;
-                }
-                border_observations_index += 1;
             }
         }
 
         observations[observations.len() - 1] = if is_arm_ready() { Fix::ZERO } else { Fix::ONE };
 
+        // for (i, o) in observations.iter().enumerate() {
+        //     print!("{o} ");
+        //     if i % N == N - 1 {
+        //         println!("");
+        //     }
+        // }
+        // println!("");
+    
         observations
-    }
-
-    fn motor_is_ready(&mut self, step_direction: Direction) {
-        self.robot_position.take_step(step_direction);
-        match step_direction {
-            Direction::Front => motor_step_fw(),
-            Direction::Back => motor_step_bw(),
-            Direction::Right => motor_turn_right(),
-            Direction::Left => motor_turn_left()
-        }
     }
 
     fn step(&mut self) {    
         radar_wait();
-        self.radar_is_ready();
-        let observations = self.get_observations();
+        let scan = radar_scan_7x7();
+        // print_scan(&scan);
+        let observations = self.get_observations(&scan);
         let nn_output = KARTOFFEL_NN.forward(observations);
         let nn_move = argmax(&nn_output).expect("nn output is empty");
-        println!("nn move: {}", nn_move);
+        // println!("nn move: {nn_move}");
         match nn_move {
             nn_move@0..=3 => {
-                let step_direction = match nn_move {
-                    0 => Direction::Front,
-                    1 => Direction::Back,
-                    2 => Direction::Left,
-                    3 => Direction::Right,
+                motor_wait();
+                match nn_move {
+                    0 => motor_step_fw(),
+                    1 => motor_step_bw(),
+                    2 => motor_turn_left(),
+                    3 => motor_turn_right(),
                     _ => unreachable!()
                 };
-                motor_wait();
-                self.motor_is_ready(step_direction);
             }
             4 => {
                 arm_wait();
                 arm_stab();
             }
+            5 => (),
             _ => unreachable!()
         }
     }
 
-    fn analyze_scan(&mut self, scan: &RadarScan<9>) -> (EnemyPositions, Borders) {
-        let mut enemy_positions = EnemyPositions::new();
-        let mut borders = Borders::new();
-
-        let n = threat_map::N as i8;
-        for y in -n/2..=n/2 {
-            for x in -n/2..=n/2 {
-                    if x == 0 && y == 0 {
-                    // we are not an enemy
-                    continue;
-                }
-                let c = scan.at(x, y);
-                let coord = Coordinate::new(x, y).orientate_north(self.robot_position.orientation);
-                if c == '@' {
-                    let id = scan.bot_at(x, y).unwrap();
-                    enemy_positions.push(EnemyPosition::new(id, coord));
-                }
-                if c == ' ' {
-                    borders.set_border(coord);
-                }
-            }
-        }
-
-        (enemy_positions, borders)
-    }
-
-    fn new(orientation: Orientation) -> Self {
-        Robot{ enemy_positions: EnemyPositions::new(), previous_enemy_positions: EnemyPositions::new(), robot_position: RobotPosition {position: Coordinate::new(0,0), orientation}, borders: Borders::new() }
+    fn new() -> Self {
+        Robot{ }
     }
 }
 
 #[no_mangle]
 fn main() {
-    let orientation = Orientation::from_integer(compass_dir() as i32 - 1).unwrap();
-
-    let mut robot = Robot::new(orientation);
+    let mut robot = Robot::new();
     loop {
         robot.step();
     }
